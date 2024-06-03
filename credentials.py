775084e3a97d25bb54689f4dd8850c661e18b122
baigorriajuanp <baigorriajuanp@gmail.com>
2024-06-02 23:30:39 -0300

    Carga de pre-entrega. Revisar credenciales hardcodeadas.

--- credentials.py
+++
@@ -1,4 +0,0 @@
-host = "http://data-engineer-cluster.cyhh5bfevlmn.us-east-1.redshift.amazonaws.com/"
-port = 5439
-data_base = "data-engineer-database" 
-
---
+++ etl.sql
@@ -0,0 +1,12 @@
+-- Tabla creada en Redshift mediante consola SQL de Dbeaver
+
+
+CREATE TABLE crypto_data (
+    id VARCHAR(50) NOT NULL,
+    symbol VARCHAR(10) NOT NULL,
+    name VARCHAR(100),
+    current_price DECIMAL(18, 8),
+    market_cap BIGINT,
+    total_volume BIGINT,
+    timestamp TIMESTAMP NOT NULL
+);
